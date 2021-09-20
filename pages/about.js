7168d2a46a654d3a82b597a07974ce13977dc28e
dnnzz <l1611012074@stud.sdu.edu.tr>
2021-09-20 14:03:33 +0300

    layout&project setup

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout";
+
+export default function AboutPage() {
+  return (
+    <Layout title="About DJ Events">
+      <h1>About</h1>
+      <p>Dj events app</p>
+    </Layout>
+  );
+}
---
+++ pages/events/[slug].js
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router";
+import Layout from "../../components/Layout";
+export default function EventPage() {
+  const router = useRouter();
+
+  return (
+    <Layout>
+      <h1>My Event</h1>
+    </Layout>
+  );
+}
