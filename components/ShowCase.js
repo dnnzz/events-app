3b9a98609b95c938738070da3c22f30b607b9ff0
dnnzz <l1611012074@stud.sdu.edu.tr>
2021-09-24 17:23:20 +0300

    data-fetching examples

---
+++ components/ShowCase.js
@@ -0,0 +1,9 @@
+import styles from "@/styles/ShowCase.module.css";
+export default function ShowCase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Welcome to party !</h1>
+      <h2>Find the DJ Events</h2>
+    </div>
+  );
+}
---
+++ pages/404.js
@@ -0,0 +1,19 @@
+import Layout from "@/components/Layout";
+import styles from "@/styles/404.module.css";
+import Link from "next/link";
+import { FaExclamationTriangle } from "react-icons/fa";
+
+export default function NotFoundPage() {
+  return (
+    <Layout title="Page not found">
+      <div className={styles.error}>
+        <h1>
+          <FaExclamationTriangle />
+          404
+        </h1>
+        <h4>Nothing here !</h4>
+        <Link href="/">Go back home page</Link>
+      </div>
+    </Layout>
+  );
+}
